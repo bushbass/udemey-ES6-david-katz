754be7244eb7104848332fa31e71e58c9849b07c
Alex <alex.nielsen@gmail.com>
2017-11-17 21:44:49 -0500

    prototype portion

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log("hello world from webpack with a third auto update");
+function Wizard(name, house, pet) {
+    this.name = name;
+    this.house = house; 
+    this.pet = pet;
+    this.greet = () =>  { return `I'm ${this.name} from ${this.house}`}
+}
+
+let harry = new Wizard("Harry Potter", "Gryffindor", "Owl")
+
+Wizard.prototype.petName;
+harry.petName = "Hedwig"
+
+
+Wizard.prototype.info = function() {
+    return `I have a ${this.pet} named ${this.petName}`
+}
+
+
+console.log(harry.greet())
+console.log(harry.info())
