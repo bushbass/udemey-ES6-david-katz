86d44fbd56068b8a4a0a32487ba0ca8529046949
Alex <alex.nielsen@gmail.com>
2017-11-17 20:38:03 -0500

    calculator class

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-console.log("hello world from webpack with a third auto update");
+class Calculator {
+    static multiply(a, b) {
+        return a * b;
+    }
+    static add(a, b) {
+        return a + b;
+    }
+}
+let a = Calculator.add(5,7)
+console.log(a)
